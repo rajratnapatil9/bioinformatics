f0fab111688c11d2df1d929e22800ed5d621a280
rajratnapatil9 <48688451+rajratnapatil9@users.noreply.github.com>
2021-04-07 02:44:36 -0400

    downloadui1

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import streamlit as st
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
+import base64
 
 # Web App Title
 st.markdown('''
@@ -15,8 +16,39 @@ This is **EDA App** used for visualizating medical data.
 with st.sidebar.header('1. Upload your CSV data'):
     uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
     st.sidebar.markdown("""
-[Example CSV input file](https://github.com/rajratnapatil9/Datasets/blob/master/heart.csv)
+[Sample:Heart-Disease-Dataset](https://github.com/rajratnapatil9/Datasets/blob/master/heart.csv)
+
 """)
+def download_link(object_to_download, download_filename, download_link_text):
+    """
+    Generates a link to download the given object_to_download.
+
+    object_to_download (str, pd.DataFrame):  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv, some_txt_output.txt
+    download_link_text (str): Text to display for download link.
+
+    Examples:
+    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
+    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
+
+    """
+    if isinstance(object_to_download,pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
+
+    # some strings <-> bytes conversions necessary here
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+
+
+# Examples
+df = pd.DataFrame({'x': list(range(10)), 'y': list(range(10))})
+st.write(df)
+
+if st.button('Download Dataframe as CSV'):
+    tmp_download_link = download_link(df, 'https://github.com/rajratnapatil9/Datasets/blob/master/heart.csv', 'Heart Deasease Data')
+    st.markdown(tmp_download_link, unsafe_allow_html=True)
+
 
 # Pandas Profiling Report
 if uploaded_file is not None:
