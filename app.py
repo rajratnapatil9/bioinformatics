dc29485081db88c05b2cc445a6060a9ae3ce9067
rajratnapatil9 <48688451+rajratnapatil9@users.noreply.github.com>
2021-04-07 03:30:14 -0400

    downloadui-cancel

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import base64
 st.markdown('''
 # **BioInformatics-EDA**
 This is **EDA App** used for visualizating medical data.
-Download sample Datasets below: 
+
 ---
 ''')
 
@@ -17,36 +17,11 @@ Download sample Datasets below:
 with st.sidebar.header('1. Upload your CSV data'):
     uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
     st.sidebar.markdown("""
-[Sample:Heart-Disease-Dataset](https://github.com/rajratnapatil9/Datasets/blob/master/heart.csv)
+Download sample Datasets below: 
+[Sample:Heart-Disease-Dataset](https://github.com/rajratnapatil9/Datasets/blob/master/heart.csv) [or Kaggle](https://archive.ics.uci.edu/ml/datasets/Heart+Disease) 
 
 """)
-def download_link(object_to_download, download_filename, download_link_text):
-    """
-    Generates a link to download the given object_to_download.
 
-    object_to_download (str, pd.DataFrame):  The object to be downloaded.
-    download_filename (str): filename and extension of file. e.g. mydata.csv, some_txt_output.txt
-    download_link_text (str): Text to display for download link.
-
-    Examples:
-    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
-    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
-
-    """
-    if isinstance(object_to_download,pd.DataFrame):
-        object_to_download = object_to_download.to_csv(index=False)
-
-    # some strings <-> bytes conversions necessary here
-    b64 = base64.b64encode(object_to_download.encode()).decode()
-
-    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
-
-hrt = pd.read_csv("https://www.kaggle.com/rashikrahmanpritom/heart-attack-analysis-prediction-dataset?select=heart.csv")
-st.write(hrt)
-
-if st.button('Heart Deasease Data'):
-    tmp_download_link = download_link(hrt, 'heart.csv', 'Heart Deasease Data')
-    st.markdown(tmp_download_link, unsafe_allow_html=True)
 
 
 # Pandas Profiling Report
